cadc4f78c1d801fbfd88f32af54800d140584786
Yasser Nabil <ynabil14@gmail.com>
2021-11-22 15:35:40 +0100

    iframe update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'phoneInteract';
+  link = "http://amazon.co.uk"; 
+
+  LoadPage(smtg){ 
+    document.querySelector('iframe').src = smtg;
+   }
 }

ed7d6af9d7aacdfb708bba2dc40cde7c63e1564d
Yasser Nabil <ynabil14@gmail.com>
2021-11-24 14:32:24 +0100

    update iframe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'phoneInteract';
-  link = "http://amazon.co.uk"; 
 
-  LoadPage(smtg){ 
-    document.querySelector('iframe').src = smtg;
+  ngOnInit(): void {
+    //this.stop();
+  }
+
+
+  loadPage(link:string){ 
+    if(link.substring(0,4) === "www.")
+    {
+      link = "https://"+link
+    }
+    else if((link.substring(8) !== "https://" || link.substring(7) !== "http://" ) && link.substring(0,3) !== "www.")
+    {
+      link = "https://www."+link
+    }
+    //console.log(link);
+    document.querySelector('iframe').src = link;
    }
+
+  back(){
+    window.history.back();
+  }
+  
+  forward(){
+    window.history.forward();
+  }
+
+  stop(){
+    console.log(window.location.href);
+    //document.querySelector('iframe').src = "";
+  }
 }
